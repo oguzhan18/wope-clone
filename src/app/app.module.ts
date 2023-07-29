e2c2826ce81a9092990accc85da7bd5055b0758d
oguzhan18 <oguzhancart1@gmail.com>
2023-07-29 09:46:37 +0300

    notifer-compoenent-feature: notfier added and build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './sharred/navbar/navbar.component';
+import { NotifierComponent } from './sharred/notifier/notifier.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    NotifierComponent
   ],
   imports: [
     BrowserModule,
